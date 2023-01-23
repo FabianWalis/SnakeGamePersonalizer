2af8427cd84ba2f36f3e3a7406c109265ade456a
fabia <fabia@DESKTOP-1QER3K5>
2023-01-23 10:48:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.CognitiveServices.Personalizer;
+using Microsoft.Azure.CognitiveServices.Personalizer.Models;
+
+using System;
+using System.Linq;
+
+namespace SnakeGamePersonalizer
+{
+    class Programm
+    {
+        private static readonly string ApiKey = "20814a5e180f4087afa64030bc8c51a6";
+        private static readonly string ApiEndpoint = "https://snakegamepersonalizer.cognitiveservices.azure.com/";
+    }
+}
+
